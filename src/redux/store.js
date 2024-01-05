597fb20790bf6a9eb802f881654a7ff68a35194a
Iryna <78929283+IrynaYrrr@users.noreply.github.com>
2024-01-05 14:22:18 +0100

    refactor store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,49 @@
+
+
+
+// export const store = configureStore({
+//   reducer:{
+//     contacts: contactReducer,
+//     filter: filterReducer,
+//   }
+// });
+
 import { configureStore } from '@reduxjs/toolkit';
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import { authReducer } from './auth/slice';
 import { contactReducer } from './contacts/contactSlice';
 import { filterReducer } from './contacts/filterSlice';
 
 
+const authPersistConfig = {
+  key: 'auth',
+  storage,
+  whitelist: ['token'],
+};
+
 export const store = configureStore({
-  reducer:{
+  reducer: {
+    auth: persistReducer(authPersistConfig, authReducer),
     contacts: contactReducer,
     filter: filterReducer,
-  }
+  },
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+  devTools: process.env.NODE_ENV === 'development',
 });
+
+export const persistor = persistStore(store);
