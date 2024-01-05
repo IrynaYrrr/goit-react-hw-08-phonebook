1d22f7621d6fc2e755f4e9923f5a28a480c0645d
Iryna <78929283+IrynaYrrr@users.noreply.github.com>
2024-01-05 12:17:58 +0100

    add operations, seletors, slice files

---
+++ src/redux/auth/operations.js
@@ -0,0 +1,28 @@
+import axios from 'axios';
+import { createAsyncThunk } from '@reduxjs/toolkit';
+
+
+
+axios.defaults.baseURL = 'https://connections-api.herokuapp.com/';
+
+const setAuthHeader = token => {
+  axios.defaults.headers.common.Authorization = `Bearer ${token}`;
+};
+
+const clearAuthHeader = () => {
+  axios.defaults.headers.common.Authorization = '';
+};
+
+
+export const register = createAsyncThunk(
+  'auth/register',
+  async (credentials, thunkAPI) => {
+    try {
+      const res = await axios.post('/users/signup', credentials);
+      setAuthHeader(res.data.token);
+      return res.data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.message);
+    }
+  }
+);
---
+++ src/redux/auth/slice.js
@@ -0,0 +1,26 @@
+import { createSlice } from '@reduxjs/toolkit';
+import { register } from './operations';
+
+
+const initialState = {
+  user: { name: null, email: null },
+  token: null,
+  isLoggedIn: false,
+  isRefreshing: false,
+};
+
+
+const authSlice = createSlice({
+  name: 'auth',
+  initialState,
+  extraReducers: builder => {
+    builder
+      .addCase(register.fulfilled, (state, action) => {
+        state.user = action.payload.user;
+        state.token = action.payload.token;
+        state.isLoggedIn = true;
+      })
+  },
+});
+
+export const authReducer = authSlice.reducer;
