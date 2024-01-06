85130927d4ee6c78be6ad61d0865d90b5b631615
Iryna <78929283+IrynaYrrr@users.noreply.github.com>
2024-01-06 17:20:22 +0100

    refactor styles

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -21,11 +21,11 @@ export const LoginForm = () => {
     <form className={css.form} onSubmit={handleSubmit} autoComplete="off">
       <label className={css.label}>
         Email
-        <input type="email" name="email" />
+        <input className={css.loginput} type="email" name="email" />
       </label>
       <label className={css.label}>
         Password
-        <input type="password" name="password" />
+        <input className={css.loginput} type="password" name="password" />
       </label>
       <button type="submit" className={css.loginbtn}>Log In</button>
     </form>
