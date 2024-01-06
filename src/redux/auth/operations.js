584daa9c0f37dd6365259694d96dfcefff0920e4
Iryna <78929283+IrynaYrrr@users.noreply.github.com>
2024-01-06 15:47:05 +0100

    refactor files

--- src/redux/auth/operations.js
+++ src/redux/auth/operations.js
@@ -1,8 +1,6 @@
 import axios from 'axios';
 import { createAsyncThunk } from '@reduxjs/toolkit';
 
-
-
 axios.defaults.baseURL = 'https://connections-api.herokuapp.com/';
 
 const setAuthHeader = token => {
