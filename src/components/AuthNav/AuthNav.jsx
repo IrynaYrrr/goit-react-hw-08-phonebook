4e3329eba9020e848e3b0b9617f31dc5beade558
Iryna <78929283+IrynaYrrr@users.noreply.github.com>
2024-01-07 12:18:52 +0100

    fix NavLink className

--- src/components/AuthNav/AuthNav.jsx
+++ src/components/AuthNav/AuthNav.jsx
@@ -5,10 +5,10 @@ import css from './AuthNav.module.css';
 export const AuthNav = () => {
   return (
     <div>
-      <NavLink className={css.link} to="/register">
+      <NavLink  className={({isActive})=>(isActive? [css.link, css.active].join(" ") : css.link)} to="/register">
         Register
       </NavLink>
-      <NavLink className={css.link} to="/login">
+      <NavLink  className={({isActive})=>(isActive? [css.link, css.active].join(" ") : css.link)} to="/login">
         Log In
       </NavLink>
     </div>
